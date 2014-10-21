b5b634439f5d9d2bb2e75fc78d6e50d6b4f5b411
Fung Wei Tan Tai <fungwei@goget.my>
2014-10-21 10:48:40 +0800

    add and fix all methods for task class

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,14 +1,28 @@
+require_relative '../../config/application'
+
 class Task < ActiveRecord::Base
 	def start!
 	    self.started_at = Time.now
-  	end
+  end
 
-	def complete!
-	    self.completed_at = Time.now
-  	end
+  def self.start!(id)
+	    temptask = self.find(id)
+	    temptask.started_at = Time.now
+	    temptask.save
+  end
 
-  	def self.new_task(desc)
+	def self.complete!(id)
+	    temptask = self.find(id)
+	    temptask.completed_at = Time.now
+	    temptask.save
+  end
+
+  def self.delete!(id)
+	    self.find(id).destroy
+  end
+
+  def self.new_task(desc)
   		self.create(description: desc)
-  	end
+  end
 
 end
