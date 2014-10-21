1f49d7cb712fc6caf86c8fd2d293d9818d7b01df
Fung Wei Tan Tai <fungwei@goget.my>
2014-10-21 10:05:10 +0800

    added methods to models/task.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,14 @@
 class Task < ActiveRecord::Base
+	def start!
+	    self.started_at = Time.now
+  	end
+
+	def complete!
+	    self.completed_at = Time.now
+  	end
+
+  	def self.new_task(desc)
+  		self.create(description: desc)
+  	end
+
 end
